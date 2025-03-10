fa7c39514e31bdd7b6ace53be91293b3f5a0c5c9
Danfix-lex <danielojolex@gmail.com>
2025-03-10 17:39:58 +0100

    commit

---
+++ StudentCourseManagementSystemLatestVersionDatabase/StudentCourseManagementSystemLatestVersion (1)/StudentCourseManagementSystem/models/user/__init__.py
@@ -0,0 +1,20 @@
+import bcrypt
+# from email_validator import validate_email, EmailNotValidError
+
+class User:
+    def __init__(self, user_id:int, username: str, password: str, role: str):
+        self.user_id = user_id
+        self.username = username
+        self.password = self._hash_password(password)
+        self.role = role
+
+    def _hash_password(self, password: str) -> bytes:
+        if isinstance(password, str):
+            return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        return password
+
+    def verify_password(self, password: str) -> bool:
+        return bcrypt.checkpw(password.encode('utf-8'), self.password)
+
+    def __str__(self):
+        return f"User(ID: {self.user_id}, Username: {self.username}, Role: {self.role})"
